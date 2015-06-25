d0e30fe0fe605edcd7545442002f57d2f0215828
Greg Hoffman <greg@4walls.net>
2015-06-25 12:22:36 -0400

    Final Edits

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
-$(document).ready(function() {
-
-	var inputNumber = parseInt(prompt("Enter a number you would like to FizzBuzz."));
-	if (isNaN(inputNumber)) {
-		prompt("Enter a number.");
-	} else if (inputNumber % 1 !== 0) {
-		prompt("Enter a WHOLE number!");
+function getNumber() {
+	var yourNumber = parseInt(prompt("Give me a number you'd like to FizzBuzz"));
+	if (isNaN(yourNumber)) {
+		alert("You did not enter a number! Please enter a number");
+		return false;
 	} else {
-		fizzbuzz(inputNumber);
+		return yourNumber;
 	}
-
-function fizzbuzz() {}
-
-	for(var i = 1; i <= inputNumber; i++){
-	if (i % 3 == 0 && i % 5 ==0) {
-		document.write('FizzBuzz'+"<br />");
-	}
-	else if (i % 3 == 0) {
-		document.write('Fizz'+"<br />");
-	}
-	else if (i % 5 == 0) {
-		document.write('Buzz'+"<br />");
-	}
-	else {
-		document.write(i+"<br />");
-	};
 }
-
+ 
+function fizzbuzz(number) {
+	for(var i = 1; i <= number; i++) {
+		if (i % 15 ==0) {
+			document.write('FizzBuzz'+"<br />");
+		}
+		else if (i % 3 == 0) {
+			document.write('Fizz'+"<br />");
+		}
+		else if (i % 5 == 0) {
+			document.write('Buzz'+"<br />");
+		}
+		else {
+			document.write(i+"<br />");
+		};
+	}
+}
+ 
+$(document).ready(function() {
+ 
+	var yourNumber = false;
+	while(yourNumber == false) {
+		yourNumber = getNumber();
+	}
+	
+	fizzbuzz(yourNumber);
+ 
 });
